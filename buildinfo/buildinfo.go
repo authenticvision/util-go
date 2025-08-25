authenticvision/util-go

commit 96cfb641442714da761b1dd4207ab11a770f71f9

httpp: Fix PathValue for ServeMux
`httpp.ServeMux` uses `http.ServeMux.Handler` to find the handler to
execute. That method does not add path values to the `Request` like
`ServeHTTP` does however. See also: https://github.com/golang/go/issues/69623

Instead of `http.ServeMux.Handler`, use `http.ServeMux.ServeHTTP` and
return errors from `ServeErrHTTP` of the handler via the `Context`.

buildinfo/buildinfo.go
@@ -53,7 +53,7 @@ func init() {
 	}
 }
 
-var Handler = httpp.EmitErrorsFunc(func(w http.ResponseWriter, r *http.Request) error {
+var Handler = func(w http.ResponseWriter, r *http.Request) error {
 	// This handler is often used as startup/liveness probe, for monitoring checks, and possibly
 	// for collecting build information regularly. Disable logging to reduce noise.
 	httpmw.DisableAccessLog(r)
@@ -68,4 +68,4 @@ var Handler = httpp.EmitErrorsFunc(func(w http.ResponseWriter, r *http.Request) 
 		GitCommitDate: GitCommitDate,
 		Version:       Version,
 	})
-})
+}

httpp/handler.go
@@ -1,6 +1,7 @@
 package httpp
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
@@ -20,37 +21,9 @@ type StdHandler interface {
 	Handler
 }
 
-// EmitErrors adds a stdlib-compatible ServeHTTP method for use with http.Handler.
-// Any errors are silently forwarded to the client and not processed further.
-func EmitErrors(handler Handler) StdHandler {
-	return &emitHandler{next: handler}
-}
-
-// EmitErrorsFunc wraps a HandlerFunc with a stdlib-compatible ServeHTTP method.
-// Any errors are silently forwarded to the client and not processed further.
-func EmitErrorsFunc(handlerFunc HandlerFunc) StdHandler {
-	return &emitHandler{next: handlerFunc}
-}
-
 // NeverErrors panics when a Handler returns a non-nil error.
 func NeverErrors(handler Handler) StdHandler {
 	return &noErrorHandler{next: handler}
-}
-
-var _ StdHandler = &emitHandler{}
-
-type emitHandler struct {
-	next Handler
-}
-
-func (h *emitHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := h.ServeErrHTTP(w, r); err != nil {
-		WriteError(w, err)
-	}
-}
-
-func (h *emitHandler) ServeErrHTTP(w http.ResponseWriter, r *http.Request) error {
-	return h.next.ServeErrHTTP(w, r)
 }
 
 var _ StdHandler = &noErrorHandler{}
@@ -69,4 +42,31 @@ func (h *noErrorHandler) ServeErrHTTP(w http.ResponseWriter, r *http.Request) er
 		panic(fmt.Errorf("unexpected error from handler: %w", err))
 	}
 	return nil
+}
+
+// CollectErrors wraps a Handler in an http.Handler that stores errors
+// returned from the wrapped handler in the Request context.
+func CollectErrors(handler Handler) http.Handler {
+	return &collectHandler{next: handler}
+}
+
+func WithErrorCollector(r *http.Request) (*http.Request, *error) {
+	var err error
+	ctx := context.WithValue(r.Context(), collectedErrorTag{}, &err)
+	return r.WithContext(ctx), &err
+}
+
+var _ http.Handler = &collectHandler{}
+
+type collectedErrorTag struct{}
+
+type collectHandler struct {
+	next Handler
+}
+
+func (h *collectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h.next.ServeErrHTTP(w, r); err != nil {
+		errPtr := r.Context().Value(collectedErrorTag{}).(*error)
+		*errPtr = err
+	}
 }

httpp/servemux.go
@@ -17,11 +17,7 @@ type ServeMux struct {
 }
 
 func (mux *ServeMux) Handle(pattern string, handler Handler) {
-	if h, ok := handler.(http.Handler); ok {
-		mux.next.Handle(pattern, h)
-	} else {
-		mux.next.Handle(pattern, EmitErrors(handler))
-	}
+	mux.next.Handle(pattern, CollectErrors(handler))
 }
 
 func (mux *ServeMux) HandleFunc(pattern string, handlerFunc HandlerFunc) {
@@ -41,14 +37,9 @@ func (mux *ServeMux) ServeErrHTTP(w http.ResponseWriter, r *http.Request) error 
 		w.WriteHeader(http.StatusBadRequest)
 		return nil
 	}
-	handlerStd, _ := mux.next.Handler(r)
-	handler, ok := handlerStd.(Handler)
-	if !ok {
-		// for HandleStd handlers or generic handlers of http.ServeMux, e.g. NotFoundHandler
-		handlerStd.ServeHTTP(w, r)
-		return nil
-	}
-	return handler.ServeErrHTTP(w, r)
+	r, errPtr := WithErrorCollector(r)
+	mux.next.ServeHTTP(w, r)
+	return *errPtr
 }
 
 // StripPrefix is copied from Go 1.24.5's http.StripPrefix, with error forwarding added.

commit 127216a5c1c54961d9477ac3f92486f32d19b027

buildinfo: Fix handler type
This broke in 96cfb64.

buildinfo/buildinfo.go
@@ -53,7 +53,7 @@ func init() {
 	}
 }
 
-var Handler = func(w http.ResponseWriter, r *http.Request) error {
+var Handler httpp.HandlerFunc = func(w http.ResponseWriter, r *http.Request) error {
 	// This handler is often used as startup/liveness probe, for monitoring checks, and possibly
 	// for collecting build information regularly. Disable logging to reduce noise.
 	httpmw.DisableAccessLog(r)