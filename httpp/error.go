8be2a6907f6588d2c62bcb9852c6c225ab8641e2
Oliver Kuckertz <oliver.kuckertz@authenticvision.com>
2025-11-06 13:04:48 +0100

    httpp: Make Error private
    This struct only implements standard interfaces and has no public
    fields. There is no need to expose it at all.

--- httpp/error.go
+++ httpp/error.go
@@ -14,20 +14,20 @@ type PublicMessage string
 const DefaultMessage PublicMessage = ""
 
 func Err(err error, statusCode int, msg PublicMessage) error {
-	return Error{
-		err:           err,
-		statusCode:    statusCode,
+	return httpError{
+		err:        err,
+		statusCode: statusCode,
 		msg:        msg,
 	}
 }
 
-type Error struct {
-	err           error
-	statusCode    int
+type httpError struct {
+	err        error
+	statusCode int
 	msg        PublicMessage
 }
 
-func (e Error) Error() string {
+func (e httpError) Error() string {
 	var sb strings.Builder
 	_, _ = fmt.Fprintf(&sb, "http status %d", e.statusCode)
 	if e.msg != "" {
@@ -39,15 +39,15 @@ func (e Error) Error() string {
 	return sb.String()
 }
 
-func (e Error) Unwrap() error {
+func (e httpError) Unwrap() error {
 	return e.err
 }
 
-func (e Error) StatusCode() int {
+func (e httpError) StatusCode() int {
 	return e.statusCode
 }
 
-func (e Error) StatusText() string {
+func (e httpError) StatusText() string {
 	if e.msg != "" {
 		return string(e.msg)
 	}
@@ -60,7 +60,7 @@ func WriteError(w http.ResponseWriter, err error) {
 	if err == nil {
 		panic("httpp.WriteError called with nil error")
 	}
-	var httpErr Error
+	var httpErr httpError
 	if errors.As(err, &httpErr) {
 		http.Error(w, httpErr.StatusText(), httpErr.StatusCode())
 	} else if err != nil {
