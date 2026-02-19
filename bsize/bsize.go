db87bae9c0cb51bdcc026549f2b68642b5957149
Oliver Kuckertz <oliver.kuckertz@authenticvision.com>
2026-02-19 11:34:26 +0100

    bsize: Move fmtutil bytes and byteflag code to bsize.go
    Not really useful as-is, but keeps the diff of the next commit with
    actual logic changes small.

--- bsize/bsize.go
+++ bsize/bsize.go
@@ -1,10 +1,41 @@
-package fmtutil
+package bsize
 
 import (
 	"fmt"
 	"regexp"
 	"strconv"
+
+	"github.com/spf13/pflag"
 )
+
+var _ pflag.Value = (*Bytes)(nil)
+
+type Bytes uint64
+
+func (b *Bytes) UnmarshalText(s []byte) error {
+	n, err := ParseBytes(string(s))
+	if err != nil {
+		return err
+	}
+	*b = Bytes(n)
+	return nil
+}
+
+func (b *Bytes) Set(s string) error {
+	return b.UnmarshalText([]byte(s))
+}
+
+func (b *Bytes) Type() string {
+	return "bytes"
+}
+
+func (b *Bytes) Bytes() uint64 {
+	return uint64(*b)
+}
+
+func (b *Bytes) String() string {
+	return FormatBytes(uint64(*b))
+}
 
 // ParseBytes parses a string of bytes size with an optional unit suffix (e.g. 1024 or 4KiB) into a number of bytes.
 // Not safe against overflows.
--- bsize/bsize_test.go
+++ bsize/bsize_test.go
@@ -1,4 +1,4 @@
-package fmtutil
+package bsize
 
 import (
 	"testing"
